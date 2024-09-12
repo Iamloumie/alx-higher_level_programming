d50f94ab23ee3a9842c10be781dc9c1d288c808c
iamloumie <lawaladedamolaolumide@gmail.com>
2024-09-12 12:59:19 +0000

    edit readme

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+# Check if the program runs as main not an import
 if __name__ == "__main__":
     from add_0 import add
 
+    # enter the values of the integers a and b
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
