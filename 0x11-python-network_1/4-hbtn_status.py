119cda9732acf9d8da74a57a86a32ca39be6ce78
Iamloumie <lawaladedamolaolumide@gmail.com>
2025-01-17 18:37:21 +0100

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-""""""
+"""Python Script that fetches https://alx-intranet.hbtn.io/status"""
+
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+
+    print("Body response:")
+    print("\t- type:", type(req.text))
+    print("\t- content:", req.text)
